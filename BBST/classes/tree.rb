729cca790556141f4ca4068c4b614e7b8c3e3c93
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-01-25 16:11:12 -0500

    Added build_tree method.
    Added method to generate a tree based on an ordered array.

--- BBST/classes/tree.rb
+++ BBST/classes/tree.rb
@@ -1,1 +1,27 @@
 # frozen_string_literal: true
+
+# Imports
+require './BBST/classes/node'
+
+# Tree class
+class Tree
+  def initialize(array)
+    @root = build_tree(array.uniq.sort)
+  end
+
+  def build_tree(array)
+    case array.count
+    when 0 then nil
+    when 1 then Node.new(array[0])
+    else
+      mid = array.count / 2
+      Node.new(array[mid], build_tree(array[0...mid]), build_tree(array[(mid + 1)..]))
+    end
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left: true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", is_left: false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", is_left: true) if node.left
+  end
+end
--- BBST/main.rb
+++ BBST/main.rb
@@ -1,1 +1,7 @@
 # frozen_string_literal: true
+
+# Imports
+require "./BBST/classes/tree"
+
+tree = Tree.new([7, 15, 6, 1, 4, 10, 9])
+tree.pretty_print

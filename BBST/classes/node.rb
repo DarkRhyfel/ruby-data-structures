c3d0990fcf250c024f1c1ea929a3db802c2e12a8
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-01-25 14:37:04 -0500

    Added basic classes.
    Added node and tree class. Node class includes the basic definition.

---
+++ BBST/classes/node.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Node class
+class Node
+  include Comparable
+
+  attr_reader :value, :left, :right
+
+  def initialize(value, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
+
+  def <=>(other)
+    value <=> other.value
+  end
+end

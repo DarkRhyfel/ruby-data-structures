adae0b176a9bb95d482af512badb4f86143a6383
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-01-26 17:54:18 -0500

    Tile class definition.
    Definition of tile class with its properties.

--- Knights/classes/tile.rb
+++ Knights/classes/tile.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+# Tile class: represents a square on the board
+class Tile
+  attr_reader :position, :parent
+  attr_accessor :children
+
+  def initialize(position, parent = nil, children = nil)
+    @position = position
+    @parent = parent
+    @children = children
+  end
+end
